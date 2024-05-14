aeb2cf7ef178e4d56c2a9a4152075e5ca5f9e3a8
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-05-15 00:07:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	let no1=0;
+	let no2=1;
+	let sum=0;
+	while(true){
+	 if(num==0){
+		 console.log(sum)
+		 break;
+	 }
+		sum=no1+no2;
+		no1=no2;
+		no2=sum;
+		num--;
+	}
 }
 
 module.exports = fibonacci;
