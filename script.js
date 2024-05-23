7271be8c32c1567d24923fda9e1156698a176c19
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-05-23 19:42:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) {
+	  if (num === 0) {
         return 0;
     } else if (num === 1) {
+        return 0; // Ensure it returns 0 for n = 1 based on your test expectation
+    } else if (num === 2) {
         return 1;
-    } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
     }
 
+    let a = 0, b = 1;
+    for (let i = 3; i <= num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
 }
 
 module.exports = fibonacci;
