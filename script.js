148d86eddda556083b55da5cd0439403bb96a8a0
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-05-23 17:44:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function fibonacci(num) {
 // your code here
-	if(num==0)return 0;
-	if(num==1)return 1
-	return fibonacci(num-1)+fibonacci(num-2);
+	if(num>1){
+	return num*fibonacci(num-1);
+}else{
+		return 1;
+}
 }
 
 module.exports = fibonacci;
